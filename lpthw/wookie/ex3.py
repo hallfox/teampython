f7ecf0ac262b1f762907f03af2021f91f70846d6
Westley Isbell <coolwest@gmail.com>
2014-07-03 14:49:32 -0500

    WikiWookie's LPTHW
    My LPTHW work

---
+++ lpthw/wookie/ex3.py
@@ -0,0 +1,36 @@
+#Prints the string between the quotations
+print "I will now count my chickens:"
+
+#prints Hens and Roosters after the prints calculates the prob
+print "Hens" , 25 + 30 / 6
+#percentage = modulus = returns remainder from division
+print "Roosters", 100 - 25 * 3 % 4
+
+#prints the string
+print "Now I will count the eggs:"
+
+#calculates the prob - PEMDAS
+#parentheses, exponents, multi, divi, add, subtract - python follows
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#prints string
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#returns boolean TRUE or FALSE
+#5 < -2 is false
+print 3 + 2 < 5 - 7
+
+#prints string then prob
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+#prints string
+print "Oh, that's why it's False."
+
+#prints string
+print "How about some more."
+
+#prints strings with boolean logic after each string
+print "Is it greater?", 5 > -2
+print "Is it great or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
---
+++ lpthw/wookie/myage.py
@@ -0,0 +1,4 @@
+print "My age is "
+print 2014-1987
+
+#remember use quit() to exit python in power shell
