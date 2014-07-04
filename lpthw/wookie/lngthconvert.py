81e28e1525c15ab03cf09fa43185fde16bb3d161
Westley Isbell <coolwest@gmail.com>
2014-07-04 11:49:34 -0500

    ex6 and fixed lngthconvert
    I had lngthconvert's % formatting wrong

--- lpthw/wookie/lngthconvert.py
+++ lpthw/wookie/lngthconvert.py
@@ -5,5 +5,18 @@ lbs = 1
 kilos = lbs / 0.453592
 
 
-print "There are", centimeters,"centimeters in", inches, "inch."
-print lbs, "lbs =", kilos, "kilos."
+print "There are %r centimeters in %r inch." % (centimeters, inches)
+print "%r lbs = %f kilos." % (lbs, kilos)
+
+
+#########other way kqr - sent me this
+
+inches = 1
+centimeters = inches * 2.54
+
+lbs = 1
+kilos = lbs / 0.453592
+
+
+print "There are {} centimeters in {} inch.".format(centimeters, inches)
+print "{} lbs = {} kilos.".format(lbs, kilos)
